dc54c3ae52f173a767f858aef06d25f7938708cb
mauriciomolinapicco <mauriscode@gmail.com>
2026-04-06 21:06:43 -0300

    selection sort c++

--- algoritmos1_up/bit_manipulation/all_substrings_of_string.cc
+++ algoritmos1_up/bit_manipulation/all_substrings_of_string.cc
@@ -6,13 +6,13 @@ using namespace std;
 
 int main() {
     string s = "hello world";
-    n = s.length();
-    n_combinations = pow(2, n);
+    int n = s.length();
+    int n_combinations = pow(2, n);
 
 
     for (int x=0; x<n_combinations; x++) {
         for (int b=0; b<n; b++) {
-            bit_prendido = (x & (1 << b))
+            int bit_prendido = (x & (1 << b))
             if (bit_prendido) {
                 cout << s[b];
             }
